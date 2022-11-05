6faf9db59dbf9d74e4b3d3c07723af43caf794cd
Ruhan Bello <ruhancvbello@gmail.com>
2022-11-05 02:33:59 -0300

    Added gizmos to see waypoints path

--- Assets/Scripts/Waypoints/WaypointsBuilder.cs
+++ Assets/Scripts/Waypoints/WaypointsBuilder.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
 public class WaypointsBuilder : MonoBehaviour {
+    [SerializeField] private bool _drawGizmos;
+
     public void Awake() {
         // the last waypoint will never have a next one, of course
         for (int i = 0; i < transform.childCount - 1; i++) {
@@ -9,4 +11,22 @@ public class WaypointsBuilder : MonoBehaviour {
             waypoint.SetWaypoint(nextWaypoint);
         }
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos() {
+        if(_drawGizmos == false) {
+            return;
+        }
+
+        for (int i = 0; i < transform.childCount; i++) {
+            var currentPos = transform.GetChild(i).position;
+            Gizmos.DrawWireSphere(currentPos, .5f);
+
+            if(i < transform.childCount - 1) {
+                var nextPos = transform.GetChild(i + 1).position;
+                Gizmos.DrawLine(currentPos, nextPos);
+            }
+        }
+    }
+#endif
 }
