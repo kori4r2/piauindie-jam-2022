b5ee1e9d5fdf5fd4d79f607e2eb4e9a5432fb63a
Ítalo <italo.ts4895@gmail.com>
2022-11-06 18:08:50 -0300

    Stop spawning workers in paths that have been completed

--- Assets/Scripts/Units/UnitSpawner.cs
+++ Assets/Scripts/Units/UnitSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Toblerone.Toolbox;
 
 public class UnitSpawner : MonoBehaviour {
     [Header("Config")]
@@ -10,16 +11,46 @@ public class UnitSpawner : MonoBehaviour {
     [SerializeField, Range(0, 20f)] private float firstSpawnDelay = 2f;
     [Header("References")]
     [SerializeField] private AIUnitRuntimeSet unitsRuntimeSet;
+    [SerializeField] private BuildingPlaceRuntimeSet buildingPlaces;
+    [SerializeField] private IntEventSO buildingCompleted;
+    private GenericEventListener<int> buildingCompletedListener;
     [Header("Debug")]
     [SerializeField] private bool _drawGizmos;
     [SerializeField] private Color _gizmosColor;
 
     private int currentUnitIndex = 0;
-    private int currentPositionIndex = 0;
+    private int currentPositionIndex = -1;
     private float currentTimer;
+    private bool[] isSpawnPointActive;
 
     private void Awake() {
         currentTimer = firstSpawnDelay;
+        isSpawnPointActive = new bool[spawnPositions.Count];
+        SetSpawnPointsStatus(true);
+        buildingCompletedListener = new GenericEventListener<int>(buildingCompleted, CheckPathsCompleted);
+    }
+
+    private void SetSpawnPointsStatus(bool newStatus) {
+        for (int index = 0; index < isSpawnPointActive.Length; index++) {
+            isSpawnPointActive[index] = newStatus;
+        }
+    }
+
+    private void CheckPathsCompleted(int pathID) {
+        SetSpawnPointsStatus(false);
+        foreach (BuildingPlace buildingPlace in buildingPlaces) {
+            if (buildingPlace.PathID < 0 || buildingPlace.PathID >= spawnPositions.Count)
+                continue;
+            isSpawnPointActive[buildingPlace.PathID] |= !buildingPlace.IsComplete;
+        }
+    }
+
+    private void OnEnable() {
+        buildingCompletedListener.StartListeningEvent();
+    }
+
+    private void OnDisable() {
+        buildingCompletedListener.StopListeningEvent();
     }
 
     private void Update() {
@@ -49,9 +80,18 @@ public class UnitSpawner : MonoBehaviour {
         if (spawnPositions.Count <= 0)
             return Vector2.zero;
 
-        Vector2 position = spawnPositions[currentPositionIndex++];
-        currentPositionIndex %= spawnPositions.Count;
+        CalculateNextValidIndex();
+        Vector2 position = spawnPositions[currentPositionIndex];
         return new Vector3(position.x, position.y, 0);
+    }
+
+    private void CalculateNextValidIndex() {
+        int count = 0;
+        do {
+            currentPositionIndex++;
+            currentPositionIndex %= spawnPositions.Count;
+            count++;
+        } while (!isSpawnPointActive[currentPositionIndex] && count < spawnPositions.Count);
     }
 
 #if UNITY_EDITOR
