169e5f1e2909f7afdcd44a3c35b0ae3693bd377b
Ítalo <italo.ts4895@gmail.com>
2022-11-05 23:09:43 -0300

    Fix to sprite flip logic

--- Assets/Scripts/Units/UnitAnimation.cs
+++ Assets/Scripts/Units/UnitAnimation.cs
@@ -11,22 +11,28 @@ public class UnitAnimation {
     private const string attackAnimString = "Attack";
     private const string isMovingAnimString = "isMoving";
     private bool lookingToTheRight;
+    private Unit thisUnit;
 
     public void Setup(Unit unit) {
         unit.OnTakeDamage.AddListener(_ => animator.SetTrigger(takeDamageAnimString));
         unit.OnAttack.AddListener(() => animator.SetTrigger(attackAnimString));
         lookingToTheRight = true;
+        thisUnit = unit;
     }
 
     public void Update(Vector2 currentSpeed) {
-        if (ShouldTurnAround(currentSpeed.x))
+        if (ShouldTurnAround(currentSpeed.x)) {
+            lookingToTheRight = !lookingToTheRight;
             spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
         animator.SetBool(isMovingAnimString, currentSpeed.sqrMagnitude > float.Epsilon);
         float speed = Mathf.Clamp(currentSpeed.magnitude, expectedSpeed / 2f, expectedSpeed * 2f);
         animator.SetFloat(speedAnimString, speed / expectedSpeed);
     }
 
     private bool ShouldTurnAround(float speedX) {
+        if (Mathf.Abs(speedX) < thisUnit.Stats.MoveSpeed * Mathf.Cos(Mathf.Deg2Rad * 89))
+            return false;
         return (lookingToTheRight && speedX < 0) || (!lookingToTheRight && speedX > 0);
     }
 }
